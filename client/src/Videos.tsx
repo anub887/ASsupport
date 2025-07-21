833dc22d9cb6098946bbc2e498f8bb841e3b093a
anub887 <anu887@gmail.com>
2025-07-21 15:20:11 +0100

    Update Videos.tsx

--- client/src/Videos.tsx
+++ client/src/Videos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import VideoLibrarySection from "../components/video-library-section";
+import VideoLibrarySection from "./components/video-library-section";
 
 export default function Videos() {
   return <VideoLibrarySection />;
