1ab8a4c44dd30ef8752ea13e71de6ad2edea53c2
anub887 <anu887@gmail.com>
2025-07-21 18:28:33 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "wouter";
+import { Route, Router } from "wouter";
 import { AuthProvider } from "./contexts/AuthContext";
 import NavBar from "./components/NavBar";
 import Home from "./Home";
@@ -11,14 +11,16 @@ import Signup from "./Signup";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavBar />
-      <Route path="/" component={Home} />
-      <Route path="/videos" component={Videos} />
-      <Route path="/resources" component={Resources} />
-      <Route path="/chat" component={Chat} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <NavBar />
+        <Route path="/" component={Home} />
+        <Route path="/videos" component={Videos} />
+        <Route path="/resources" component={Resources} />
+        <Route path="/chat" component={Chat} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </AuthProvider>
+    </Router>
   );
 }
