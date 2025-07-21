47a9c645f6adc03cb4a9780a3037e6561a2088fb
anub887 <anu887@gmail.com>
2025-07-21 18:42:14 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { AuthProvider } from "./contexts/AuthContext";
 import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
-    <div style={{ padding: "2rem", fontSize: "1.2rem", color: "black" }}>
-      ✅ NavBar Test Below
-      <NavBar />
-    </div>
+    <AuthProvider>
+      <div style={{ padding: "2rem", fontSize: "1.2rem", color: "black" }}>
+        ✅ NavBar + AuthProvider
+        <NavBar />
+      </div>
+    </AuthProvider>
   );
 }
