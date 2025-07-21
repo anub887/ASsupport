02f002c33757997bb7d17c378b1c72af483facb5
anub887 <anu887@gmail.com>
2025-07-21 18:56:08 +0100

    Update Home.tsx

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import VideoLibrarySection from "./components/video-library-section";
 
 export default function Home() {
   return (
     <div style={{ padding: "2rem", fontSize: "1.5rem", color: "black" }}>
-      ✅ Home Page Loaded Successfully!
+      ✅ Testing VideoLibrarySection
+      <VideoLibrarySection />
     </div>
   );
 }
